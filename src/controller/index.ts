a7cc2deb289d11e7e7c3719941c9b36cd3e5cc16
SAI SATHWIK REDDY VARIKOTI <varikotisaisathwikreddy@gmail.com>
2024-04-18 12:03:13 -0400

    test cronjob

--- src/controller/index.ts
+++ src/controller/index.ts
@@ -3,6 +3,7 @@ import { forgotPasswordMail } from "./forgotpasswordmail";
 import { eventRegistrationConfirmationMail } from "./eventRegistrationConfirmation";
 
 const healthCheck = (req: any, res: any) => {
+    console.log("Job is running");
     res.status(200).send("Health check passed");
 };
 
